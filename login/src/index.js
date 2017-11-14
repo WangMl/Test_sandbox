f018dba59058850427763893d9aefe9eea5dd88d
xinlu.wang <xinlu.wang@yunshipei.com>
2017-11-14 16:32:07 +0800

    fixing

--- login/src/index.js
+++ login/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
-
+const { Title } = AMUI2;
 const styles = {
   fontFamily: 'sans-serif',
   textAlign: 'center',
@@ -9,6 +9,7 @@ const styles = {
 
 const App = () => (
   <div style={styles}>
+    <Title>dsds</Title>
     <Hello name="CodeSandbox" />
     <h2>Start wxl editing to see some magic happen {'\u2728'}</h2>
   </div>
